28c643a0544d49c1935a41edb0813f65e91cf8d7
gaurav <raveformails@gmail.com>
2012-10-17 15:42:08 +0530

    starting mybook app development

---
+++ mybook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybook::Application.config.secret_token = '6b999f0edda1e1a258b541452a8148e232cfb9e51d28d27bde982897a08b03694cf721946f4db98c41499bb7f6c269063fb30a948b0ec1bfbd71c0fde79fdd30'
